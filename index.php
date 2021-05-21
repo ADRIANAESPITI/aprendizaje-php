4f6c2358c527087638beba856e27d64fb28ce907
ADRIANAESPITI <70924531+ADRIANAESPITI@users.noreply.github.com>
2021-05-21 13:23:15 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>MVC en PHP</title>
+</head>  
+</body>
+</html>
+       
+    <?php
+        require_once('controlador/Productos_controlador.php');
+    ?>
+
+</body>
+</html>
